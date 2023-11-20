9fff5da103cbeb52e93ff123035f18b1e13032c6
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-20 17:35:53 +0300

    Initial Commit

---
+++ src/test/java/com/workshop/dixie/CloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.dixie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/workshop/dixie/TestCloudServiceApplication.java
@@ -0,0 +1,23 @@
+package com.workshop.dixie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestCloudServiceApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(CloudServiceApplication::main).with(TestCloudServiceApplication.class).run(args);
+    }
+
+}
