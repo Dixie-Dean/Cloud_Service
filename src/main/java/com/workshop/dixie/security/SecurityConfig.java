00a15a93fcc29bf318437af98400e7c1b543a522
Dixie Dean <d.w.sokolov26@gmail.com>
2024-03-01 13:38:50 +0300

    chore: add JWT entry point and access denied handler

--- src/main/java/com/workshop/dixie/security/SecurityConfig.java
+++ src/main/java/com/workshop/dixie/security/SecurityConfig.java
@@ -3,13 +3,12 @@ package com.workshop.dixie.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,8 +23,8 @@ import java.util.Arrays;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig implements WebMvcConfigurer {
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
+    private final JwtEntryPoint entryPoint;
+    private final JwtAccessDeniedHandler accessDeniedHandler;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -45,9 +44,11 @@ public class SecurityConfig implements WebMvcConfigurer {
                     auth.requestMatchers("/cloud/register").permitAll();
                     auth.anyRequest().authenticated();
                 })
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                        .authenticationEntryPoint(this.entryPoint)
+                        .accessDeniedHandler(this.accessDeniedHandler))
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
